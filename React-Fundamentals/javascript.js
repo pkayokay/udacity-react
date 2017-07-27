2e771a5e6802a15812cb8577947f580bb2a5260b
Paul Kim <pkim.dev@gmail.com>
2017-07-26 23:42:14 -0400

    map and filter

---
+++ React-Fundamentals/javascript.js
@@ -0,0 +1,91 @@
+// React is just Javascript...
+const friends = ['mike','rich','paul'];
+
+<ul>
+  <li>{friends[0]}</li>
+  <li>{friends[1]}</li>
+  <li>{friends[2]}</li>
+</ul>
+
+<ul>
+  {friends.map(name => (
+    <li>{name}</li>
+  ))}
+</ul>
+
+
+// .map() method
+const friends = ['mike','rich','paul'];
+const friendsLength = friends.map(name => name.length);
+// returns a new array, does not modify original.
+// [4,4,4]
+
+/* Quiz Using .map()
+ *
+ * Using the musicData array and .map():
+ *   - return a string for each item in the array in the following format
+ *     <album-name> by <artist> sold <sales> copies
+ *   - store the returned data in a new albumSalesStrings variable
+ *
+ * Note:
+ *   - do not delete the musicData variable
+ *   - do not alter any of the musicData content
+ *   - do not format the sales number, leave it as a long string of digits
+ */
+
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    { artist: 'Original Broadway Cast Recording',
+      name: 'Hamilton: An American Musical', sales: 820000 },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const albumSalesStrings = musicData.map(function(i) {
+  return `${i.name} by ${i.artist} sold ${i.sales} copies`
+});
+
+console.log(albumSalesStrings);
+
+
+
+// .filter() method
+const names = ['mike','rich','paul'];
+const shortNames = names.filter(name => name.length < 5);
+/* Quiz Using .filter()
+ *
+ * Using the musicData array and .filter():
+ *   - return only album objects where the album's name is
+ *     10 characters long, 25 characters long, or anywhere in between
+ *   - store the returned data in a new info variable
+ *
+ * Note:
+ *   - do not delete the musicData variable
+ *   - do not alter any of the musicData content
+ */
+
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    { artist: 'Original Broadway Cast Recording',
+      name: 'Hamilton: An American Musical', sales: 820000 },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const results = musicData.filter(function(album) {
+  return album.name.length >= 10 && album.name.length <= 25;
+})
+
+console.log(results);
