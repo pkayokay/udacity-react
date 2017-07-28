dbe4babe01e9f2acdbfc6359a4b1d90bb6c5f2ad
Paul Kim <pkim.dev@gmail.com>
2017-07-27 21:43:55 -0400

    creating an app

---
+++ Rendering-UI/creating.js
@@ -0,0 +1,15 @@
+// We can scaffold an app, and have webpack set up
+// Webpack is a code bundler, that allows JSX to be transpiled into regular JavaScript
+
+  ~'npm install -g create-react-app'
+  // sets up everything with no need for configuration!
+  // Gives global acccess to use command 'create-react-app'
+
+  ~'create-react-app myapp'
+  // installs react, react-dom and react-scripts
+  // react-scripts gives us babel, webpack, webpack-dev-server
+
+  // Refer to https://learn.tylermcginnis.com/courses/enrolled/50507 for in depth explanation of project structure
+
+
+// Yarn Package Manager: package manager (an alternative) similar to NPM. 'yarn start' to start the development server.
